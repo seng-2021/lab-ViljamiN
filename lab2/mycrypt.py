c5f442a45cdb05476513830d142431576f7aa0fc
Viljami <viljami.narinen@aalto.fi>
2024-02-13 23:27:58 +0200

    #2 - testing for invalid characters fixed

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -7,16 +7,25 @@ def encode(s):
     origlen = len(s)
     crypted = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=', '!"#€%&/()=1234567890'))
+    invalid_characters = {'\xe5', '\xe4', '\xf6'}
+
     if len(s) > 1000:
         raise ValueError
+
     for c in s:
-        if c.isalpha():
+        if c in invalid_characters:
+            # in this case, the character is not allowed
+            raise ValueError
+        elif c.isalpha():
             if c.islower():
                 c = c.upper()
             # Rot13 the character for maximum security
-            crypted += codecs.encode(c, 'rot13')
+            crypted += codecs.encode(c, 'rot_13')
         elif c in digitmapping:
             crypted += digitmapping[c]
+        else:
+            # in this case, the character is not allowed
+            raise ValueError
 
     return crypted
 
