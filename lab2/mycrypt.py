8568f7369a8dd25e0b81288418311e0924cf41f9
Viljami <viljami.narinen@aalto.fi>
2024-02-13 23:02:04 +0200

    #1 - test_encode_decode fails fixed

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,24 +1,38 @@
 import codecs
 
+
 def encode(s):
-    if not isinstance(s,str):
+    if not isinstance(s, str):
         raise TypeError
     origlen = len(s)
     crypted = ""
-    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    digitmapping = dict(zip('1234567890!"#€%&/()=', '!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
     for c in s:
         if c.isalpha():
             if c.islower():
-                c=c.upper()
+                c = c.upper()
             # Rot13 the character for maximum security
-            crypted+=codecs.encode(c,'rot13')
+            crypted += codecs.encode(c, 'rot13')
         elif c in digitmapping:
-          crypted+=digitmapping[c]
+            crypted += digitmapping[c]
 
     return crypted
 
-def decode(s):
-    return s
 
+def decode(s):
+    if not isinstance(s, str):
+        raise TypeError
+    origlen = len(s)
+    decrypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=', '!"#€%&/()=1234567890'))
+    for c in s:
+        if c.isalpha():
+            if c.isupper():
+                c = c.lower()
+            decrypted += codecs.decode(c, 'rot_13')
+        elif c in digitmapping:
+            decrypted += digitmapping[c]
+
+    return decrypted
